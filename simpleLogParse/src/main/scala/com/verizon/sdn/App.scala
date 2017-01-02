247e5018b15ad1f3c5884d9b884b108d12372344
Madhuri Jagannath <madhurijagannath@gmail.com>
2017-01-02 13:54:51 -0600

    20170102: git commit without add

--- simpleLogParse/src/main/scala/com/verizon/sdn/App.scala
+++ simpleLogParse/src/main/scala/com/verizon/sdn/App.scala
@@ -6,7 +6,8 @@ package com.verizon.sdn
 object App {
   
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
+
+
   def main(args : Array[String]) {
     println( "Hello World!" )
     println("concat arguments = " + foo(args))
